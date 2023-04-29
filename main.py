01ac56f926da68aee04480f9c9e300a8eaffef0a
Vincenzo Incutti <vincenzo.incutti@gmail.com>
2023-04-29 14:07:36 +0100

    search loop

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import openai
 from ai import AI
 
 # Set up OpenAI API key
-openai.api_key = "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"
-youtube_api_key = 'AIzaSyAUL3RG9_REUaKmb9L-q3MhHbFQgF9R_AI'
+openai.api_key = "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"
+youtube_api_key = 'AIzaSyBA43hoEQmwUEBynYWjQKsazVnRr6ybFb0'
 
 
 query = input("Enter your search query: ")
 
 youtube_search = YouTubeSearch(youtube_api_key)
 
-search_response = youtube_search.search_videos(query)
+# search_response = youtube_search.search_videos(query)
+youtube_search.search_loop(query)
 
-if search_response:
-    youtube_search.save_json_to_file(search_response)
-
-# ai = AI(system="You answer questions whihc are jumbled up",openai_module=openai)
-# response, messages = ai.generate_response("MSYDNYEATDEHNOADIGHSNAIWROUTUAY", voice=False)
+# if search_response:
+#     youtube_search.save_json_to_file(search_response)
